d18668850466871b0b85a27b9375be9f895bea43
Simon A. F. Lund <os@safl.dk>
2025-09-24 08:04:33 +0200

    feat: add meld and diff-tool in git config
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- tasks/300-configure-git.sh
+++ tasks/300-configure-git.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
-set -euo pipefail
-
-# Configure global git identity.
-# Usage: ./install-git.sh
+# Configure git globally
+# 
 # Customize via env: GIT_NAME="Your Name" GIT_EMAIL="you@example.com" ./install-git.sh
-
-GIT_EMAIL="${GIT_EMAIL:-"os@safl.dk"}"
-GIT_NAME="${GIT_NAME:-"Simon A. F. Lund"}"
+set -euo pipefail
 
 if ! command -v git >/dev/null 2>&1; then
   echo "Error: git not found in PATH." >&2
   exit 1
 fi
 
+GIT_EMAIL="${GIT_EMAIL:-"os@safl.dk"}"
+GIT_NAME="${GIT_NAME:-"Simon A. F. Lund"}"
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name  "${GIT_NAME}"
+git config --global core.editor "hx"
+git config --global diff.tool meld
+git config --global difftool.prompt false
+git config --global merge.tool meld
+git config --global mergetool.prompt false
 
 echo "# git-config"
 git config list
