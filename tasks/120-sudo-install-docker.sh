c271fea85a9affc765f2628624fef06536d31e2d
Simon A. F. Lund <os@safl.dk>
2026-02-23 20:43:43 +0100

    fix(tasks): correct apt sources.list.d path in Docker install
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- tasks/120-sudo-install-docker.sh
+++ tasks/120-sudo-install-docker.sh
@@ -21,7 +21,7 @@ install_docker_apt() {
   echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] \
     https://download.docker.com/linux/${ID} ${VERSION_CODENAME} stable" \
-    > /etc/apt/sources-list.d/docker.list
+    > /etc/apt/sources.list.d/docker.list
 
   apt-get -y update
   apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
